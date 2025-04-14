2327488a29b937ed6005652268f80652a1e3abeb
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-14 01:13:13 -0500

    added logic for more than one unique element

--- lonelyInteger.js
+++ lonelyInteger.js
@@ -8,7 +8,26 @@ function lonelyInteger(arr) {
     return result;
 };
 
-let arr = [1,4,5,4,5,6,6,8]
+let arr = [1, 2, 3, 2, 1, 4, 5];
 
 let response = lonelyInteger(arr)
-console.log(response);
+// console.log(response);
+
+function getAllUniqueIntegers(arr) {
+    let count = {};
+    for (let i = 0; i < arr.length; i++) {
+        count[arr[i]] = (count[arr[i]] || 0) + 1
+    }
+    
+    let repeats = [];
+
+    for (let element in count) {
+        
+        if( count[element] === 1){
+            repeats.push(Number(element))
+        }
+    }
+    console.log(repeats);
+}
+
+getAllUniqueIntegers(arr)
