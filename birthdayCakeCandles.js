7b0ff3184405c9115cc01fd2cd2a244fb46a0314
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-19 12:48:01 -0500

    Added another approach with no sorting

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -16,6 +16,22 @@ function birthdayCakeCandles(candles) {
     
 }
 
+function birthdayCakeCandlesNoSort(candles) {
+    let maxHeight = 0;
+    let count = 0;
 
+    for (let i = 0; i < candles.length; i++) {
+        if(candles[i] >  maxHeight) {
+            maxHeight = candles[i]
+            count++;
+        } else if(candles[i] ==  maxHeight) {
+            count++
+        }
+    }
+    console.log(count);
+    
+
+}
 
 birthdayCakeCandles([3, 2, 1, 3])
+birthdayCakeCandlesNoSort([3, 2, 1, 3])
