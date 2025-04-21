516dd7f3bf3083b9077c9956f99e3421587f351f
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-21 11:56:49 -0500

    other way to do it

--- medianOfTwoArrays.js
+++ medianOfTwoArrays.js
@@ -17,3 +17,19 @@ var findMedianSortedArrays = function (nums1, nums2) {
 let nums1 = [1, 2], nums2 = [3, 4]
 
 findMedianSortedArrays(nums1, nums2)
+
+
+var findMedianSortedArraysSimple = function(nums1, nums2) {
+    const merged = [...nums1, ...nums2].sort((a, b) => a - b);
+    const len = merged.length;
+
+    if (len % 2 === 1) {
+        // Odd length, return the middle element
+        return merged[Math.floor(len / 2)];
+    } else {
+        // Even length, average the two middle elements
+        const mid1 = merged[len / 2 - 1];
+        const mid2 = merged[len / 2];
+        return (mid1 + mid2) / 2;
+    }
+};
