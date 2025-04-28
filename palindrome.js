9fdc4f3cd41316265ade704398a75872b062bb0d
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-28 15:21:50 -0500

    Reverse integer

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+var isPalindrome = function (int) {
+
+  let num, reversed = 0;
+  let original = int
+  if (int < 0) {
+    return false;
+  }
+  while (int !== 0) {
+    num = int % 10
+    reversed = reversed * 10 + num;
+    int = Math.trunc(int / 10)
+  }
+  return reversed == original;
+};
+
+console.log(isPalindrome(-10));
+
