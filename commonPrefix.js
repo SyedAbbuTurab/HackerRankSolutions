d9ca40b50c83be932bbdd3440bb3ae73b41db1e8
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-29 17:28:58 -0500

    common prefix of strs of array

---
+++ commonPrefix.js
@@ -0,0 +1,22 @@
+var longestCommonPrefix = function (strs) {
+
+    if (strs.length === 0) return "";
+
+    for (let i = 0; i < strs.length; i++) {
+        const initials = strs[0][i]
+        for (let j = 0; j < strs.length; j++) {
+
+            if (i >= strs[j].length || strs[j][i] !== initials) {
+                return strs[0].substring(0, i)
+            }
+
+        }
+
+    }
+    return strs[0]
+
+};
+
+
+let res = longestCommonPrefix(["flower", "flow", "light"])
+console.log(res);
