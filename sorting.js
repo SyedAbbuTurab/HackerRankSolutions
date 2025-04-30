592ec67f54165fc476630e2864132db8e7c1e220
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-30 15:43:44 -0500

    added swapped tech to for more faster

--- sorting.js
+++ sorting.js
@@ -1,21 +1,35 @@
 
 // Bubble sort
 
-var bubbleSort = function (arr) {
+var bubbleSort = function (arr, bool) {
     let arrLength = arr.length;
     let temp;
-
-    for (let i = 0; i < arrLength; i++) {
-        for (let j = 0; j < arrLength - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    // Two ways to do it see if block
+    if (bool) {
+        for (let i = 0; i < arrLength; i++) {
+            for (let j = 0; j < arrLength - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
+    } else {
+        for (let i = 0; i < arrLength; i++) {
+            let swapped = false;
+            for (let j = 0; j < arrLength - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                    swapped = true
+                }
+            }
+            if (!swapped) break;
+        }
     }
+
     return arr;
 }
 
-console.log(bubbleSort([5, 2, 9, 1]));
+console.log(bubbleSort([5, 2, 9, 1], false));
 
