50e2db354d7bd0fb7691754af8d8ea14b401cff4
Abbu Turab <syedabbuturab786@gmail.com>
2025-05-10 10:55:00 -0500

    updated condition for returning thr array

--- searchRange.js
+++ searchRange.js
@@ -5,15 +5,15 @@
  */
 var searchRange = function (nums, target) {
 
-    if(nums.length < 0) return [-1, -1];
+    if (nums.length < 0) return [-1, -1];
 
     let res = []
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] == target) {
             res.push(i)
-        }   
+        }
     }
-    return res
+    return (res.length) === 0 ? [-1, -1] : [res[0], res[res.length - 1]]
 };
 
 console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
