d72161f84171bbd1be9d00038659afbaef8ce75b
Abbu Turab <syedabbuturab786@gmail.com>
2025-05-12 16:42:38 -0500

    can jump from array position

---
+++ canJump.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let maxReach = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(i > maxReach) return false;
+        maxReach = Math.max(maxReach, i + nums[i])
+        if(maxReach >= nums.length - 1) return true
+    }
+};
+
+console.log(canJump([2,3,1,1,4])); // true
+console.log(canJump([3,2,1,0,4])); // false
