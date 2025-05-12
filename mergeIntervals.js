20e424db46d0725dadde998e9941564be52e7d65
Abbu Turab <syedabbuturab786@gmail.com>
2025-05-12 16:51:55 -0500

    merge intervals

---
+++ mergeIntervals.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intervals.length <= 1) return intervals;
+
+    // Step 1: Sort intervals based on the start time
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const merged = [intervals[0]];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const lastMerged = merged[merged.length - 1];
+        const current = intervals[i];
+
+        // If current interval overlaps with the last one, merge them
+        if (current[0] <= lastMerged[1]) {
+            lastMerged[1] = Math.max(lastMerged[1], current[1]);
+        } else {
+            merged.push(current);
+        }
+    }
+
+    return merged;
+};
+
