2838dfb9af71804ea8ad16ff259d9c85a3611a55
Abbu Turab <syedabbuturab786@gmail.com>
2025-05-22 23:33:51 -0500

    formatted line

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -28,9 +28,6 @@ var lengthOfLastWord = function (s) {
 
 };
 console.log(lengthOfLastWord("Hello World"));
-console.log(
-    lengthOfLastWord("   fly me   to   the moon  ")
-
-);
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
 
 
