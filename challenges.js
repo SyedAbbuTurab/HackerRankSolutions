27c871641ccfc3256febe8c03fd809ca6d1c62eb
Abbu Turab <syedabbuturab786@gmail.com>
2025-05-24 14:03:07 -0500

    Challenge of duplicate

---
+++ challenges.js
@@ -0,0 +1,18 @@
+//🔹 Challenge: Remove Duplicates from an Array
+const removeDuplicatesArr = async (arr) => {
+
+    let setArr = new Set();
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if(!setArr.has(arr[i])) {
+            setArr.add(arr[i]);
+            result.push(arr[i])
+        }
+    }
+    console.log(result);
+
+}
+
+removeDuplicatesArr([4, 5, 9, 4, 9, 1, 5]
+)
