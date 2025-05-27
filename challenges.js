b04a479a06a94626c25e7e063198e42c58734073
Abbu Turab <syedabbuturab786@gmail.com>
2025-05-27 13:31:49 -0500

    Longest consec number in array

--- challenges.js
+++ challenges.js
@@ -74,3 +74,23 @@ const missingNumber = arr => {
 };
 
 console.log(missingNumber([3, 0, 1])); // Output: 2
+
+
+const longestConsecutive = (arr) => {
+    const numSet = new Set(arr);
+    let maxLength = 0;
+
+    for (let num of arr) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentLength = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentLength += 1;
+            }
+            maxLength = Math.max(maxLength, currentLength)
+        }
+    }
+    return maxLength;
+}
