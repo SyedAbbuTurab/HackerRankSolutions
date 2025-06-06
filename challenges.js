8ee7deda7672a706b19ce22ba431f779e123cbe8
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-05 23:26:57 -0500

    commented

--- challenges.js
+++ challenges.js
@@ -350,6 +350,6 @@ function findPairsWithSum(arr, target) {
     }
     return result
 }
-console.log(findPairsWithSum([1, 2, 3, 4, 5], 6)); // Output: [ [2, 4], [1, 5] ]
+// console.log(findPairsWithSum([1, 2, 3, 4, 5], 6)); // Output: [ [2, 4], [1, 5] ]
 
 
