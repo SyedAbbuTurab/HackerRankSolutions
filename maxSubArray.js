917d748525378920136ac0c89d698d681594729b
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-04 15:19:12 -0500

    commented logger

--- maxSubArray.js
+++ maxSubArray.js
@@ -14,4 +14,4 @@ var maxSubArray = function(nums) {
     return maxSum;
 };
 
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // Output: 6
+// console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // Output: 6
