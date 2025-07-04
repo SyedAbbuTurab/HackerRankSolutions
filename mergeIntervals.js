d6b85cf107e7100b3c4a03c3de0cb7e36fd05f3e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-04 15:18:19 -0500

    commented logger

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -25,10 +25,10 @@ var merge = function(intervals) {
     return merged;
 };
 
-console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
+// console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
 // Output: [[1,6],[8,10],[15,18]]
 
-console.log(merge([[1,4],[4,5]]));
+// console.log(merge([[1,4],[4,5]]));
 // Output: [[1,5]]
 
 /**
