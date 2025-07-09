cad06f1653bbde6e5b0b7c8441cf531344989d4e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-09 12:58:25 -0500

    test module file

---
+++ authUser.js
@@ -0,0 +1,9 @@
+var LogInUser = (name) => {
+    return `${name} has been logged in successfully!`
+}
+
+var SignUpUser =  (name) => {
+    return `${name} has been signed up successfully!`
+}
+
+module.exports = { LogInUser, SignUpUser }
